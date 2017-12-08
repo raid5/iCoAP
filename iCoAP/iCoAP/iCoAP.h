ab8ceb272c164bcf683360fe8196ed81380832c6
Adam McDonald <adam@l4digital.com>
2017-12-08 11:49:13 -0800

    Created framework project for Carthage support.

---
+++ iCoAP/iCoAP/iCoAP.h
@@ -0,0 +1,20 @@
+//
+//  iCoAP.h
+//  iCoAP
+//
+//  Created by Adam McDonald on 12/8/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iCoAP.
+FOUNDATION_EXPORT double iCoAPVersionNumber;
+
+//! Project version string for iCoAP.
+FOUNDATION_EXPORT const unsigned char iCoAPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iCoAP/PublicHeader.h>
+#import <iCoAP/GCDAsyncUdpSocket.h>
+#import <iCoAP/ICoAPExchange.h>
+#import <iCoAP/ICoAPMessage.h>
+#import <iCoAP/NSString+hex.h>
