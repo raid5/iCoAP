d79ec53b30567eb6c8f459c9efe9a366f00326fe
jaythejones <jay@assembleinc.com>
2020-01-14 16:35:23 -0800

    Fixes iOS11 breaking change with NSData description output (#2)

--- iCoAP/iCoAP/NSString+hex.m
+++ iCoAP/iCoAP/NSString+hex.m
@@ -24,10 +24,29 @@
 }
 
 + (NSString *)stringFromDataWithHex:(NSData *)data{
-    NSString *result = [[data description] stringByReplacingOccurrencesOfString:@" " withString:@""];
-    result = [result substringWithRange:NSMakeRange(1, [result length] - 2)];
-    
-    return result;
+	/** Original library implementation relied on [data description] for
+	 it's byte string. iOS 11 changed the format, so it no longer works.
+	 new format: {length = 27, bytes = 0x64859f82 50dcb4cd ff4d6574 686f6420 ... 20416c6c 6f776564 }
+	 old format: <64859f8250dcb4cdff4d6574686f642020416c6c6f776564>
+	 
+	 This snippet just walks through the byte array and formats a mutable string.
+	 -JJ 1/14/2020
+	 */
+	
+	// we first need to get the length of our hexstring
+	// data.lenght returns the lenght in bytes, so we *2 to get as hexstring
+	NSUInteger capacity = data.length * 2;
+	// Create a new NSMutableString with the correct lenght
+	NSMutableString *mutableString = [NSMutableString stringWithCapacity:capacity];
+	// get the bytes of data to be able to loop through it
+	const unsigned char *buf = (const unsigned char*) [data bytes];
+	
+	NSInteger t;
+	for (t=0; t<data.length; ++t) {
+		// "%02X" will append a 0 if the value is less than 2 digits (i.e. 4 becomes 04)
+		[mutableString appendFormat:@"%02lx", (unsigned long)buf[t]];
+	}
+	return [NSString stringWithString:mutableString];
 }
 
 + (NSString *)get0To4ByteHexStringFromInt:(int32_t)value {
